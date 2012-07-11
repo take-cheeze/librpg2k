95eb822ee03b1641785f6dd04a914ef28fe2e3f7
take_cheeze <takechi101010@gmail.com>
2012-07-11 14:31:47 +0900

    make output file changable

--- tool/analyzer.cxx
+++ tool/analyzer.cxx
@@ -4,7 +4,12 @@
 
 int main(int argc, char* argv[])
 {
-  assert(argc == 2);
+  assert(argc == 2 || argc == 3);
+
+  if(argc == 3) {
+    rpg2k::debug::ANALYZE_RESULT.close();
+    rpg2k::debug::ANALYZE_RESULT.open(argv[2], std::ios::out);
+  }
 
   std::cout << "Analyzing: " << argv[1] << std::endl;
   rpg2k::model::load_lcf(argv[1])->analyze();
