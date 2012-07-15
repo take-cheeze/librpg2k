eadd559c8ed4b99f88de25a435f2a0146224d52f
take_cheeze <takechi101010@gmail.com>
2012-07-15 23:02:24 +0900

    print usage if invalid arguments, exit if file open was failed

--- tool/analyzer.cxx
+++ tool/analyzer.cxx
@@ -11,7 +11,19 @@ int main(int argc, char* argv[])
   } else if(argc == 3) {
     rpg2k::debug::ANALYZE_RESULT.close();
     rpg2k::debug::ANALYZE_RESULT.open(argv[2], std::ios::out);
-  } else { assert(false); }
+  } else {
+    std::cout
+        << "usage> analyzer INPUT_FILE [OUTPUT_FILE]" << std::endl
+        << std::endl
+        << "- If OUTPUT_FILE is unspecified it will be outputed to stdout." << std::endl
+        ;
+    return EXIT_FAILURE;
+  }
+
+  if(!rpg2k::debug::ANALYZE_RESULT) {
+    std::cout << "file open failed" << std::endl;
+    return EXIT_FAILURE;
+  }
 
   rpg2k::model::load_lcf(argv[1])->analyze();
 
--- tool/to_json.cxx
+++ tool/to_json.cxx
@@ -11,7 +11,19 @@ int main(int argc, char* argv[])
     static_cast<std::ostream&>(ost).rdbuf(std::cout.rdbuf());
   } else if(argc == 3) {
     ost.open(argv[2]);
-  } else { assert(false); }
+  } else {
+    std::cout
+        << "usage> to_json INPUT_FILE [OUTPUT_FILE]" << std::endl
+        << std::endl
+        << "- If OUTPUT_FILE is unspecified it will be outputed to stdout." << std::endl
+        ;
+    return EXIT_FAILURE;
+  }
+
+  if(!ost) {
+    std::cout << "file open failed" << std::endl;
+    return EXIT_FAILURE;
+  }
 
   ost << rpg2k::model::load_lcf(argv[1])->to_json();
 
